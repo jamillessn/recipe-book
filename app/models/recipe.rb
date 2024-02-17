4834ec96e3c1f72ab6cf4c05db58662f42c0ba4f
jamillessn <jamillerosesison1@gmail.com>
2024-02-17 12:08:47 +0800

    update for render

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,4 @@
 class Recipe < ApplicationRecord
     validates :name, presence: true
+    validates :image_url, format: { with: URI::DEFAULT_PARSER.make_regexp, message: "must be a valid URL" }
 end
